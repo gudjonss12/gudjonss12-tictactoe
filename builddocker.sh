754e94ab943299ad04d02ed9487be9acbc78da3e
Gudjon <gudjonss12@ru.is>
2016-12-07 09:19:44 +0000

    Added comments to docker-compose.yaml and builddocker.sh

--- builddocker.sh
+++ builddocker.sh
@@ -59,8 +59,10 @@ cp ./.env ./build/
 
 # Copy the necessary files to the AWS machine.
 # TODO move this to a separate script
+echo Copying necessary files to AWS instance
 scp -i ./admin-key-key-ireland.pem ./{docker-compose.yaml,.env} ec2-user@52.51.82.250:~/
-# scp -i ./admin-key-key-ireland.pem ./.env ec2-user@52.51.82.250:~/
+echo Done copying to AWS instance
+
 
 cd build
 echo Building docker image: gudjonss12/tictactoe:$GIT_COMMIT
