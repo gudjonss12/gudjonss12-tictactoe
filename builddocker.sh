5293ea42ec2f8e2c4dd61d67bf414103ec99f7c4
Gudjon <gudjonss12@ru.is>
2016-12-08 11:42:12 +0000

    added npm install to build scrip in hopes to fix jenkins

--- builddocker.sh
+++ builddocker.sh
@@ -16,6 +16,10 @@ fi
 # Remove .git from url in order to get https link to repo (assumes https url for GitHub)
 export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
 
+# Install dependencies
+echo Installing dependencies
+npm install
+
 # Build the project
 echo Building application
 npm run build
