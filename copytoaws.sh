dde827c9daf1385d2ec6af380e3c919455b1544a
Gudjon <gudjonss12@ru.is>
2016-12-09 13:21:04 +0000

    fixed scopy from jenkins to aws

--- copytoaws.sh
+++ copytoaws.sh
@@ -4,5 +4,5 @@
 # This script is invoked after the commit stage,
 # that is after jenkins has succesfully built the program
 echo Copying necessary files to AWS instance
-scp -i ./../admin-key-key-ireland.pem ./{docker-compose.yaml,.env} ec2-user@52.51.82.250:~/
+scp -i ../admin-key-key-ireland.pem ./{docker-compose.yaml,.env} ec2-user@52.51.82.250:~/
 echo Done copying to AWS instance
