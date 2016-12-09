e43262ebbd3d7c9fd483fb1476f76fe4775270a2
Gudjon <gudjonss12@ru.is>
2016-12-09 11:42:24 +0000

    fixes to dockerbuild

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -9,7 +9,8 @@ if [ -z "$GIT_COMMIT" ]; then
   export GIT_URL=$(git config --get remote.origin.url)
 fi
 
-cd ./build
+cd ./..
+cd ./tttCommitStage/build/
 echo Building docker image: gudjonss12/tictactoe:$GIT_COMMIT
 docker build -t gudjonss12/tictactoe:$GIT_COMMIT .
 
